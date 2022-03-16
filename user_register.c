6b7c9a5dd976883365532def4178f3732c050dc0
Astoriaiii <95459670+Astoriaiii@users.noreply.github.com>
2022-03-16 11:39:09 +0800

    Add files via upload

---
+++ user_register.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void user_register {
+	
+	printf("Please enter a user name:");
+	scanf("%s", input_name);
+	printf("Please enter a password:");
+	scanf("%s", password);
+	
+}
+
+void register_check {
+	
+	File *user;
+	index = 0;
+	int i;
+	
+	user = fopen("user.txt","r");
+	while (!feof(user)){
+		fscanf(user, "%s\n", user_name);
+		index ++;
+	}
+	fclose(user);
+	
+	for (i=0; i<index; i++){
+		if (strcmp(input_name, user_name) == 0){
+			printf("Sorry, registration unsuccessful, the username you entered already exsits.");
+			break;
+		}
+		else {
+			fwrite(buffer,sizeof(char), sizeof(input_name), user);
+			fwrite(buffer,sizeof(char), sizeof(password), user);
+			printf("Congratulations for registration successful!")
+		}
+	}
+}
+
+
