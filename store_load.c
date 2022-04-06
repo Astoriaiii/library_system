92ca691465e621915f7d2b11c60cfb556ef2a2d4
Astoriaiii <95459670+Astoriaiii@users.noreply.github.com>
2022-04-07 01:36:05 +0800

    Add files via upload

--- store_load.c
+++ store_load.c
@@ -50,25 +50,17 @@ char copies[20];
        
         head_Book = (Book*)malloc(sizeof(Book));
 	head_Book->next = NULL;
-        //head_Book->next = (Book*)malloc(sizeof(Book));
-        //temp_Book = head_Book;
-        Book *end_Book = (Book*)malloc(sizeof(Book));
- 
-	
+        Book *end_Book = (Book*)malloc(sizeof(Book));	
 	head_Book  = end_Book;
-	
-        //temp_Book->next= (Book*)malloc(sizeof(Book));
-        //temp_Book = (Book*)malloc(sizeof(Book));
         while (end_Book != NULL) { 
 		Book* q_Book =  (Book*)malloc(sizeof(Book));
-                //temp_Book = (Book*)malloc(sizeof(Book));
 		title=(char*)malloc(sizeof(char)*100);
 		authors=(char*)malloc(sizeof(char)*100);
-            a = fscanf(file,"%s %s\t%s\t%s\t%s", id, title, authors, year, copies);
-fgetc(file);
+                a = fscanf(file,"%s %s\t%s\t%s\t%s", id, title, authors, year, copies);
+		fgetc(file);
             if (a != 5){
 		free((void*)end_Book);
-printf("3");
+
 		break;
                 
            }
@@ -78,28 +70,13 @@ a = atoi(year);
 q_Book->year = a;
 a = atoi(copies);
 q_Book->copies = a;
-             //temp_Book->id = atoi(temp_Book->id);
-            //temp_Book->year = atoi(temp_Book->year);
-            //temp_Book->copies = atoi(temp_Book->copies);
             q_Book->title = (char*)malloc(100*sizeof(char));
             q_Book->authors = (char*)malloc(100*sizeof(char));
 	    strcpy(q_Book->title,title);
 	    strcpy(q_Book->authors,authors);
-//printf("%d\t%d\n", temp_Book->year, temp_Book->copies);
-	   
-	    //Book *q_Book = (Book*)malloc(sizeof(Book));
 q_Book->next= NULL;
 end_Book -> next = q_Book;
 end_Book = end_Book ->next;
-printf("%d\t%s\t%s\t%d\t%d\n", end_Book->id, end_Book->title, end_Book->authors, end_Book->year, end_Book->copies);
-//temp_Book->next = (Book*)malloc(sizeof temp_Book->next);
-	//temp_Book->next = q_Book;      
-	//q_Book = temp_Book;
-            //if (head_Book == NULL){
-              //  head_Book->next = temp_Book;
-           // }
-            //else{
-              //  p_Book->next = temp_Book;
     }
 	fclose(file);
     return 0;
@@ -114,17 +91,3 @@ printf("55");
 		temp_Book = temp_Book->next;
 	}
 }
-/*
-void readBookInFile (struct node* head_node) {
-    FILE *fp = fopen("book.txt","a+");
-    Book *book;
-    if (fp != NULL) {
-        while (fscanf(fp, "%d %s %s %d %d", book->id, book->title, book->authors, book->year, book->copies) != EOF) {
-            add_node(head_node, &book);
-        }
-        fclose(fp);
-    }
-    else {
-        printf("Error occurs when open the file");
-    }
-}*/
