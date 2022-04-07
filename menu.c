d8343edc34917645b73ed928343cf7febaa01a69
Astoriaiii <95459670+Astoriaiii@users.noreply.github.com>
2022-04-07 21:45:35 +0800

    menu displayed added

--- menu.c
+++ menu.c
@@ -1,9 +1,8 @@
-#ifndef MENU_C
-#define MENU_C#include <stdio.h>
-#include "book_management.h"
+#include "menu.h"
+#include <stdio.h>
 
 //Initially list when user open the project
-void menu_initial(){
+void menu_initial() {
 	printf("Please choose an option:\n");
 	printf("1) Register an account\n");
 	printf("2) Login\n");
@@ -14,7 +13,7 @@ void menu_initial(){
 }
 
 //Menu displayed for librarian to operate books
-void menu_librarian(){
+void menu_librarian() {
 	printf("Please choose an option:\n");
 	printf("1) Add a book\n");
 	printf("2) Remove a book\n");
@@ -26,22 +25,20 @@ void menu_librarian(){
 
 //Menu displayed for user to operate books
 void menu_user() {
-    printf("Please choose an option:\n");
-    printf("1) Borrow a book\n");
-    printf("2) Return a book\n");
-    printf("3) Search for books\n");
-    printf("4) Display all books\n");
-    printf("5) Log out\n");
-    printf(" Option:\n");
+	printf("Please choose an option:\n");
+	printf("1) Borrow a book\n");
+	printf("2) Return a book\n");
+	printf("3) Search for books\n");
+	printf("4) Display all books\n");
+	printf("5) Log out\n");
+	printf(" Option:\n");
 }
 
 //Menu displayed for search books
-/* void menu_search() {
-    printf("1) search by title\n");
-    printf("2) search by author\n");
-    printf("3) search by year\n");
-    printf("4) Quit\n");
-    printf(" Option:\n");
-}*/
-
-#endif
+void menu_search() {
+	printf("1) search by title\n");
+	printf("2) search by author\n");
+	printf("3) search by year\n");
+	printf("4) Quit\n");
+	printf(" Option:\n");
+}
